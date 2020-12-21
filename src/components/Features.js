0f3a7841b8a1b62c3deec80878608880d440c37e
unknown <mathuria.rajatkumar@gmail.com>
2020-12-21 19:52:27 +0530

    updated bg

--- src/components/Features.js
+++ src/components/Features.js
@@ -89,140 +89,144 @@ function Features() {
             Our Offerings
           </h1>
         </div>
-      <div className="grid-3 container my-5">
-        <div className="bg-white shadow-lg rounded features">
-          <Card border="success">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path
-                fill="#24AE21"
-                fill-opacity="1"
-                d="M0,192L48,197.3C96,203,192,213,288,234.7C384,256,480,288,576,298.7C672,309,768,299,864,277.3C960,256,1056,224,1152,202.7C1248,181,1344,171,1392,165.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-              ></path>
-            </svg>
-            <Card.Img
-              variant="top"
-              src={basic}
-              className="rounded-circle p-4 m-1"
-              height="100%"
-              width="100%"
-            />
-            <Card.Body>
-              <Card.Title className="text-center text-success">
-                <h3>Basic</h3>
-              </Card.Title>
-              <Card.Text>
-                <ul className="container">
-                  <li>Purchase/Sale Order </li>
-                  <li>Job card</li>
-                  <li>BOM Items Purchase Order</li>
-                  <li>Dispatch- Challans</li>
-                </ul>
-              </Card.Text>
-              <Button
-                className="buynow"
-                variant="success"
-                onClick={() => setModalShow(true)}
-              >
-                Buy Now
-              </Button>
-            </Card.Body>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
-              <path
-                fill="#24AE21"
-                fill-opacity="1"
-                d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-              ></path>
-            </svg>
-          </Card>
+        <div className="grid-3 container my-5">
+          <div className="bg-white shadow-lg rounded features">
+            <Card>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path
+                  //
+                  className="basic"
+                  fill-opacity="1"
+                  d="M0,192L48,197.3C96,203,192,213,288,234.7C384,256,480,288,576,298.7C672,309,768,299,864,277.3C960,256,1056,224,1152,202.7C1248,181,1344,171,1392,165.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+                ></path>
+              </svg>
+              <Card.Img
+                variant="top"
+                src={basic}
+                className="rounded-circle p-4 m-1"
+                height="100%"
+                width="100%"
+              />
+              <Card.Body>
+                <Card.Title className="text-center">
+                  <h3 style={{ color: "#43b9f3" }}>Basic</h3>
+                </Card.Title>
+                <Card.Text>
+                  <ul className="container">
+                    <li>Purchase/Sale Order </li>
+                    <li>Job card</li>
+                    <li>BOM Items Purchase Order</li>
+                    <li>Dispatch- Challans</li>
+                  </ul>
+                </Card.Text>
+                <Button
+                  className="buynow buynow-basic"
+                  onClick={() => setModalShow(true)}
+                >
+                  Buy Now
+                </Button>
+              </Card.Body>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+                <path
+                  // fill="#24AE21"
+                  className="basic"
+                  fill-opacity="1"
+                  d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+                ></path>
+              </svg>
+            </Card>
+          </div>
+          <div className="bg-white shadow-lg rounded features">
+            <Card>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path
+                  className="standard"
+                  // fill="#ffd700"
+                  fill-opacity="1"
+                  d="M0,192L48,197.3C96,203,192,213,288,234.7C384,256,480,288,576,298.7C672,309,768,299,864,277.3C960,256,1056,224,1152,202.7C1248,181,1344,171,1392,165.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+                ></path>
+              </svg>
+              <Card.Img
+                variant="top"
+                src={standard}
+                className="rounded-circle p-4 m-1"
+                height="100%"
+                width="100%"
+              />
+              <Card.Body>
+                <Card.Title className="text-center">
+                  <h3 style={{ color: " #0276e1" }}>Standard</h3>
+                </Card.Title>
+                <Card.Text>
+                  <ul className="container py-4">
+                    <li>Paper Requisition.</li>
+                    <li>Paper Purchase Order.</li>
+                  </ul>
+                </Card.Text>
+                <Button
+                  className="buynow text-white buynow-standard"
+                  onClick={() => setModalShow(true)}
+                >
+                  Buy Now
+                </Button>
+              </Card.Body>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+                <path
+                  // fill="#ffd700"
+                  className="standard"
+                  fill-opacity="1"
+                  d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+                ></path>
+              </svg>
+            </Card>
+          </div>
+          <div className="bg-white shadow-lg rounded features">
+            <Card>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path
+                  // fill="#F32013"
+                  className="premium"
+                  fill-opacity="1"
+                  d="M0,192L48,197.3C96,203,192,213,288,234.7C384,256,480,288,576,298.7C672,309,768,299,864,277.3C960,256,1056,224,1152,202.7C1248,181,1344,171,1392,165.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+                ></path>
+              </svg>
+              <Card.Img
+                variant="top"
+                src={premium}
+                className="rounded-circle p-4 m-1"
+                height="100%"
+                width="100%"
+              />
+              <Card.Body>
+                <Card.Title className="text-center">
+                  <h3 style={{ color: "#064c8a" }}>Premium</h3>
+                </Card.Title>
+                <Card.Text>
+                  <ul className="container">
+                    <li>Quotations,Costing & Estimation</li>
+                    <li>Job Tracking & Scheduling.</li>
+                    <li>Performa Invoice</li>
+                    <li>Purchase & Sale Return.</li>
+                  </ul>
+                </Card.Text>
+                <Button
+                  className="buynow buynow-premium"
+                  onClick={() => setModalShow(true)}
+                >
+                  Buy Now
+                </Button>
+              </Card.Body>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+                <path
+                  // fill="#F32013"
+                  className="premium"
+                  fill-opacity="1"
+                  d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+                ></path>
+              </svg>
+            </Card>
+          </div>
         </div>
-        <div className="bg-white shadow-lg rounded features">
-          <Card border="warning">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path
-                fill="#ffd700"
-                fill-opacity="1"
-                d="M0,192L48,197.3C96,203,192,213,288,234.7C384,256,480,288,576,298.7C672,309,768,299,864,277.3C960,256,1056,224,1152,202.7C1248,181,1344,171,1392,165.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-              ></path>
-            </svg>
-            <Card.Img
-              variant="top"
-              src={standard}
-              className="rounded-circle p-4 m-1"
-              height="100%"
-              width="100%"
-            />
-            <Card.Body>
-              <Card.Title className="text-center text-warning">
-                <h3>Standard</h3>
-              </Card.Title>
-              <Card.Text>
-                <ul className="container py-2">
-                  <li>Paper Requisition.</li>
-                  <li>Paper Purchase Order.</li>
-                </ul>
-              </Card.Text>
-              <Button
-                variant="warning"
-                className="buynow text-white mt-5"
-                onClick={() => setModalShow(true)}
-              >
-                Buy Now
-              </Button>
-            </Card.Body>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
-              <path
-                fill="#ffd700"
-                fill-opacity="1"
-                d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-              ></path>
-            </svg>
-          </Card>
-        </div>
-        <div className="bg-white shadow-lg rounded features">
-          <Card border="danger">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path
-                fill="#F32013"
-                fill-opacity="1"
-                d="M0,192L48,197.3C96,203,192,213,288,234.7C384,256,480,288,576,298.7C672,309,768,299,864,277.3C960,256,1056,224,1152,202.7C1248,181,1344,171,1392,165.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-              ></path>
-            </svg>
-            <Card.Img
-              variant="top"
-              src={premium}
-              className="rounded-circle p-4 m-1"
-              height="100%"
-              width="100%"
-            />
-            <Card.Body>
-              <Card.Title className="text-center text-danger">
-                <h3>Premium</h3>
-              </Card.Title>
-              <Card.Text>
-                <ul className="container">
-                  <li>Quotations,Costing & Estimation</li>
-                  <li>Job Tracking & Scheduling.</li>
-                  <li>Performa Invoice</li>
-                  <li>Purchase & Sale Return.</li>
-                </ul>
-              </Card.Text>
-              <Button
-                variant="danger buynow"
-                onClick={() => setModalShow(true)}
-              >
-                Buy Now
-              </Button>
-            </Card.Body>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
-              <path
-                fill="#F32013"
-                fill-opacity="1"
-                d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-              ></path>
-            </svg>
-          </Card>
-        </div>
-      </div>
       </div>
     </div>
   );
